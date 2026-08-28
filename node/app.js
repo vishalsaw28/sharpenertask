84b180e27fe81f16fde2b14c1a2973d5ca29d198
vishal kumar <149081237+sawvishal@users.noreply.github.com>
2026-08-28 18:45:07 +0530

    add the node task1

---
+++ node/app.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+
+  if (req.url === "/home") {
+    res.end("Welcome home");
+  } else if (req.url === "/about") {
+    res.end("Welcome to About Us");
+  } else if (req.url === "/node") {
+    res.end("Welcome to my Node Js project");
+  } else {
+    res.end("Page Not Found");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
